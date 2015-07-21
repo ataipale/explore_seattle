df8f576bf5b25f1ea49ef309341c73021ae726a2
Alex Taipale <alextaipale@gmail.com>
2015-07-20 22:18:48 -0600

    adding browser config

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import json, requests
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from pymongo import MongoClient
 from bson import json_util
 # from flask.ext.pymongo import PyMongo
@@ -7,6 +7,9 @@ from bson import json_util
 client = MongoClient()
 db = client.restaurants
 coll = db.seattle_only_meta
+
+browser = request.headers.get('User-Agent')
+print browser
 
 # EB looks for an 'application' callable by default.
 app = Flask(__name__)
@@ -38,22 +41,7 @@ def search():
     return json_util.dumps(data)
 
 
-    # return open("yelp_no_rev.json", "r")
-    # return mongo.db.seattle_only_meta.find()
-    # with open("./data/seattle_no_rev.json", "r") as f:
-        # return json.load(f)
-    
-# add a rule for the index page.
-# app.add_url_rule('/', 'index', (lambda: )
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-# app.add_url_rule('/<username>', 'hello', (lambda username:
-#     header_text + say_hello(username) + home_link + footer_text))
-
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    app.debug = True
+
     app.run(host='0.0.0.0')
