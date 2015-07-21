2b89a35f0b0117f3cd497b0fd0e5639c0e1228eb
Alex Taipale <alextaipale@gmail.com>
2015-07-20 22:33:22 -0600

    testing browser warning

--- application.py
+++ application.py
@@ -2,36 +2,31 @@ import json, requests
 from flask import Flask, request, render_template, redirect
 from pymongo import MongoClient
 from bson import json_util
-# from flask.ext.pymongo import PyMongo
 
 client = MongoClient()
 db = client.restaurants
 coll = db.seattle_only_meta
 
-browser = request.headers.get('User-Agent')
-print browser
+user_agent = request.headers.get('User-Agent')
 
-# EB looks for an 'application' callable by default.
 app = Flask(__name__)
-# mongo = PyMongo(restaurants)
 
 @app.route('/')
 def homepage():
 
-    print 'homepage!'
-    return render_template('index.html')
-    # return header_text + say_hello() + instructions + footer_text
+    if "Mozilla" in user_agent:
+        return render_template('index_mozilla.html')
+    else:
+        return render_template('index.html')
 
 @app.route('/about')
 def about():
 
-    print 'about!'
     return render_template('about.html')
 
 @app.route('/contact')
 def contact():
 
-    print 'contact!'
     return redirect("http://www.alextaipale.com/about", code=302)
 
 
